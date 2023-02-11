91f96583a4b2834eb5f7cd85b7c03102f314f4c9
ethanraygor <89420452+ethanraygor@users.noreply.github.com>
2023-02-11 12:54:15 -0700

    Fix 6

--- functional-testing/cypress/e2e/front-end/state_dropdown.cy.js
+++ functional-testing/cypress/e2e/front-end/state_dropdown.cy.js
@@ -21,13 +21,16 @@ describe('State Dropdown Menu', () => {
         .should('be.visible')
     })
     it('dropdown menu has length 52', () => { //50 states plus DC and PR
-        cy.get('div[id="react-select-5-listbox"] > .css-4ljt47-MenuList')
+        cy.get('.css-tlfecz-indicatorContainer').click()
+        .get('.css-4ljt47-MenuList')
         .its('childElementCount').should('eq', '52')
     })
     it('dropdown only has valid state values', () => {
         cy.get('div[id="react-select-5-listbox"] > .css-4ljt47-MenuList > div')
-        .each((text, index, list) => {
-            expect(text.innerText).equal(possible_states[index])
+        .each(($div, index, $lis) => {
+            cy.wrap($div)
+            .its('innerText')
+            .should('eq', possible_states[index])
         })
         
     })
