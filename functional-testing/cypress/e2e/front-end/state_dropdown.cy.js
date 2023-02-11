fc556e12992237e2a2f8b9a2bc7104a2c622be71
ethanraygor <89420452+ethanraygor@users.noreply.github.com>
2023-02-11 13:33:02 -0700

    Tests Working and Code Commented

--- functional-testing/cypress/e2e/front-end/state_dropdown.cy.js
+++ functional-testing/cypress/e2e/front-end/state_dropdown.cy.js
@@ -1,3 +1,7 @@
+/**
+ * State Dropdown Menu Test Suite
+ */
+
 /// <reference types="cypress" />
 
 import { elements } from "../../fixtures/data-cy.json";
@@ -9,27 +13,30 @@ const possible_states = [
 	'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH',
 	'OK', 'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT',
 	'WA', 'WI', 'WV', 'WY'
-	]
+	] // 50 states + DC and PR in alphabetical order by code
 
-describe('State Dropdown Menu', () => {
-    beforeEach(() => {
+describe('State Dropdown Menu', () => { //test suite name
+    beforeEach(() => { //refreshes page and selects state dropdown
         cy.visit(url)
         cy.get('.css-tlfecz-indicatorContainer').eq(1).click()
     })
     it('state dropdown button works', () => {
         cy.get('div[id="react-select-5-listbox"]')
-        .should('be.visible')
+        .should('be.visible') //checks state dropdown is visible
     })
     it('dropdown menu has length 52', () => { //50 states plus DC and PR
         cy.get('div[id="react-select-5-listbox"] > .css-4ljt47-MenuList > div')
         .each(($div, index, $lis) => {
+            /*This is probably not the optimal way of doing this but is the
+              only way I could make it work. It just checks the length of 
+              the array returned by the each method. */
             expect($lis).to.have.length(52)
         })
     })
     it('dropdown only has valid state values', () => {
         cy.get('div[id="react-select-5-listbox"] > .css-4ljt47-MenuList > div')
         .each(($div, index, $lis) => {
-            expect($div.html()).equal(possible_states[index])
+            expect($div.html()).equal(possible_states[index]) //check value in dropdown aligns with value in states array
         })   
     })
 })
