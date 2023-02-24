38df8b9bbf32a8f74899d99a91a1b84e8f63726d
ethanraygor <89420452+ethanraygor@users.noreply.github.com>
2023-02-24 13:38:29 -0700

    fix beforeEach

--- functional-testing/cypress/e2e/front-end/csv_download.cy.js
+++ functional-testing/cypress/e2e/front-end/csv_download.cy.js
@@ -10,11 +10,11 @@ describe('CSV Download', () => {
     beforeEach(() => {
         cy.visit(url)
         cy.get('.css-1hb7zxy-IndicatorsContainer').eq(1).click() //year dropdown
-        cy.get('div[id="react-select-3-listbox"] > .css-4ljt47-MenuList > div[id="react-select-3-option-0"]').click(); //select first year, should be 2018
+        cy.get('div[id="react-select-3-listbox"] >> div[id="react-select-3-option-0"]').click(); //select first year, should be 2018
     })
     it('csv dowload button returns a csv file', () => {
         cy.get('.css-tlfecz-indicatorContainer').eq(1).click()
-        cy.get('div[id="react-select-5-listbox"] > .css-4ljt47-MenuList > div[id="react-select-5-option-13"]').click();
+        cy.get('div[id="react-select-5-listbox"] >> div[id="react-select-5-option-13"]').click();
         cy.get('.css-sghohy-MuiButtonBase-root-MuiButton-root').eq(0).click()
         cy.get('.css-sghohy-MuiButtonBase-root-MuiButton-root').eq(1).click()
         cy.verifyDownload('export.csv');
