e854acd534f7090da62ed8c623d5dcc77de5bcbb
ethanraygor <89420452+ethanraygor@users.noreply.github.com>
2023-02-25 10:25:17 -0700

    test using api not frontend

---
+++ functional-testing/cypress/e2e/front-end/csv_frontend.cy.js
@@ -0,0 +1,22 @@
+/**
+ * CSV Download button test suite
+ */
+/// <reference types="cypress" />
+
+import { elements } from "../../fixtures/data-cy.json";
+const url = "http://localhost:3000/f22-fires-wild/Data";
+
+describe('CSV Download', () => {
+    beforeEach(() => {
+        cy.visit(url)
+        cy.get('.css-1hb7zxy-IndicatorsContainer').eq(0).click() //year dropdown
+        cy.get('div[id="react-select-3-listbox"] >> div[id="react-select-3-option-0"]').click(); //select first year, should be 2018
+    })
+    it('csv dowload button returns a csv file', () => {
+        cy.get('.css-1hb7zxy-IndicatorsContainer').eq(1).click()
+        cy.get('div[id="react-select-5-listbox"] >> div[id="react-select-5-option-13"]').click();
+        cy.get('.css-sghohy-MuiButtonBase-root-MuiButton-root').eq(0).click()
+        cy.get('.css-sghohy-MuiButtonBase-root-MuiButton-root').eq(1).click()
+        cy.verifyDownload('export.csv');
+    })
+})
