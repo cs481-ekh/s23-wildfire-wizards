3250eea1f63cfd33f018f221e7b170379e4d3b76
ethanraygor <89420452+ethanraygor@users.noreply.github.com>
2023-02-28 10:04:31 -0700

    Implements #30 (#56)
    * new dependency cy-verify-download
    
    * first test attempt
    
    * rebuild permissions
    
    * fix beforeEach
    
    * button fix
    
    * fix test 1
    
    * download file cypress dependency
    
    * change cypress configs
    
    * idaho file test
    
    * test using api not frontend
    
    * code error
    
    * test in frontend to include search
    
    * provide url
    
    * filename fix
    
    * filename fix 2
    
    * equal fix
    
    * equal fix
    
    * asdf
    
    * fix idaho string
    
    * switch equal to contain
    
    * break apart strings
    
    * use standard includes
    
    * 2nd state test
    
    * added 3rd state
    
    * Missed quotes

--- functional-testing/cypress/support/e2e.js
+++ functional-testing/cypress/support/e2e.js
@@ -18,3 +18,6 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+// used to verify csv downloads
+require('cy-verify-downloads').addCustomCommand();
