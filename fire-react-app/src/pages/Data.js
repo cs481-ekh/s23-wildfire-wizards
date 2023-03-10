01847819348e99da86584135ec7778aa11585c2e
ethanraygor <89420452+ethanraygor@users.noreply.github.com>
2023-03-10 11:37:31 -0700

    use object

--- fire-react-app/src/pages/Data.js
+++ fire-react-app/src/pages/Data.js
@@ -818,7 +818,7 @@ const Data = () => {
                             var catMin = 0;
                           }
                           var catMax = categories_range[catIndex];
-                          modalData.map((key, val) => {
+                          Object.entries(modalData).map((key, val) => {
                             if(val=catMin){
                               //if else tree to prevent values showing up as true or false instead of 1.0 or 0.0
                               if (key[1] == 1.0) {
