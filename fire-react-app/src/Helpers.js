4ebaa08fa7ceae323d7aa00c2bb9555d66c52782
ethanraygor <89420452+ethanraygor@users.noreply.github.com>
2023-04-12 16:18:42 -0600

    base fields of order of categories

--- fire-react-app/src/Helpers.js
+++ fire-react-app/src/Helpers.js
@@ -1,8 +1,20 @@
+const allCategories = ["FPA_FOD", "Climate and Economic Justice Screening Tool", 
+  "Annual Climate", "Cheat Grass", "Climate Normals", "GRIDMENT", 
+  "Climate Percentiles", "Ecoregions", "Digital Elevation Map", "Vegetation",
+  "Risk Management Assistance", "Fire Regime Groups", "Fire Stations", 
+  "Geographic Area Coordination Centers", "Gap Analysis Project", 
+  "Gross Domestic Product", "Global Human Modification", "MODIS NDVI", 
+  "NOAA NDVI", "National Land Cover Database", "Population", "Pyrome", "Road", 
+  "Social Vulnerability Index", "Rangeland Production Monitoring Service"];
+
 export function getFields(categories) {
     var rtVal = new Array();
-    categories.forEach(c => {
-        var fields = fieldsFromCategory(c);
-        fields.forEach(f => {rtVal.push(f);});
+    allCategories.forEach(c => {
+        if(categories.contains(c)){
+            var fields = fieldsFromCategory(c);
+            fields.forEach(f => {rtVal.push(f);});
+        }
+        
     });
     return rtVal;
 }
