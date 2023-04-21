71fe8321eef001911ae3d35e58a9df39fa9bce98
ethanraygor <89420452+ethanraygor@users.noreply.github.com>
2023-04-21 10:42:11 -0600

    Implements #165 (#169)
    * add info popup
    
    * change symbol

--- fire-react-app/src/pages/Data.js
+++ fire-react-app/src/pages/Data.js
@@ -832,6 +832,15 @@ const Data = () => {
             </Grid>
           <br />
         </div>
+        <JSPopup
+          trigger={<Link>ℹ️ Search Tips ℹ️</Link>}
+          position="top center"
+        >
+          <div>
+            If you decide to change the attribute categories, you don't need to search again to see the new categories in a fire's Additional Details.
+          </div>
+        </JSPopup>
+        <br />
         {loading && (
           <div>
             <CircularProgress />
